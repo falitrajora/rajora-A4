3031e902cecfde926f29c7ffcdbb7bcdca989dac
falitrajora <50635246+falitrajora@users.noreply.github.com>
2020-08-07 17:42:10 -0400

    Add files via upload

---
+++ assets/data/myinfo.ts
@@ -0,0 +1,9 @@
+import { Rajora } from '../../app/rajora';
+
+export const MYINFO: Rajora={
+    id: 991548226,
+    name:'Falit Rajora',
+    loginName:'rajora',
+    campus:'Davis',
+    title:'Assigment 4',
+} ;
